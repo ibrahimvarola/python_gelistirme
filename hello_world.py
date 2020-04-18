db3c084ffae25335584c392b45afc918b3ad43bd
İbrahim Varola <varolaibrahim@gmail.com>
2020-04-18 22:02:44 +0300

    Başlangıç gönderisi

---
+++ hello_world.py
@@ -0,0 +1,1 @@
+print("Merhaba yalan dünya!")
---
+++ print.py
@@ -0,0 +1,9 @@
+dizi1 = "Am I still to create the perfect system?"
+dizi2 = "Hello there!"
+dizi3 = "Ah shit here we go again.."
+
+print(dizi1)
+print("dizi1' in uzunluğu:", len(dizi1))
+
+print(*dizi2, sep=".")
+ 
