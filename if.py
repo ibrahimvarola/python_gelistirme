fd70c22b82bef7b1e99eda5b2fc771aec2fd2691
İbrahim Varola <varolaibrahim@gmail.com>
2020-04-19 13:16:18 +0300

    yeni dosya:   if.py

---
+++ if.py
@@ -0,0 +1,9 @@
+k_ad = "ibrahimvarola"
+sifre = "123456"
+
+#giris_kad = input("kullanıcı adını giriniz: ")
+if (input("kullanıcı adını giriniz: ") == k_ad) & (input("şifreyi giriniz: ") == sifre):
+    print("Hoşgeldiniz!")
+
+else:
+    print("not ok!")
