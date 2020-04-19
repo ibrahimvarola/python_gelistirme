043e6f34d5c229197853f9871cb572fc38272373
İbrahim Varola <varolaibrahim@gmail.com>
2020-04-19 13:25:52 +0300

    değiştirildi: if.py

--- if.py
+++ if.py
@@ -3,7 +3,9 @@ sifre = "123456"
 
 #giris_kad = input("kullanıcı adını giriniz: ")
 if (input("kullanıcı adını giriniz: ") == k_ad) & (input("şifreyi giriniz: ") == sifre):
-    print("Hoşgeldiniz!")
+    print("""Hoşgeldiniz {}
+Bugün neler yapmak isterseniz? 
+    """.format(k_ad))
 
 else:
-    print("not ok!")
+    print("Kullanıcı adı veya şifre hatalı!")
