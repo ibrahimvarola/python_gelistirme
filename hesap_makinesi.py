3e77a6a5c72cf4d3142e9a2cd30cb9d80de31f93
İbrahim Varola <varolaibrahim@gmail.com>
2020-04-19 15:45:56 +0300

    yeni dosya:   hesap_makinesi.py

---
+++ hesap_makinesi.py
@@ -0,0 +1,42 @@
+giriş = """
+1- Topla
+2- Çııkar
+3- Çarp
+4- Böl
+5- Kare Hesapla
+6- Karekök Hesapla
+"""
+
+print(giriş)
+soru = input("Lütfen yapmak istediğiniz işlemi seçiniz: ")
+
+if soru == "1":
+    sayi1 = int(input("Lütfen birinci sayıyı giriniz: "))
+    sayi2 = int(input("Lütfen ikinci sayıyı giriniz: "))
+    print (sayi1, "+", sayi2, "=", sayi1+sayi2)
+
+elif soru == "2":
+    sayi1 = int(input("Lütfen birinci sayıyı giriniz: "))
+    sayi2 = int(input("Lütfen ikinci sayıyı giriniz: "))
+    print (sayi1, "-", sayi2, "=", sayi1-sayi2)
+
+elif soru == "3":
+    sayi1 = int(input("Lütfen birinci sayıyı giriniz: "))
+    sayi2 = int(input("Lütfen ikinci sayıyı giriniz: "))
+    print (sayi1, "*", sayi2, "=", sayi1*sayi2)
+
+elif soru == "4":
+    sayi1 = int(input("Lütfen birinci sayıyı giriniz: "))
+    sayi2 = int(input("Lütfen ikinci sayıyı giriniz: "))
+    print (sayi1, "/", sayi2, "=", sayi1/sayi2)
+
+elif soru == "5":
+    sayi1 = int(input("Lütfen karesi alınacak sayıyı giriniz: "))
+    print (sayi1, "^ 2 =", sayi1 ** 2)
+
+elif soru == "6":
+    sayi1 = int(input("Lütfen karekökü alınacak sayıyı giriniz: "))
+    print (sayi1, "^ 0.5 =", sayi1 ** 0.5)
+
+else:
+    print("Yanlış giriş.")
