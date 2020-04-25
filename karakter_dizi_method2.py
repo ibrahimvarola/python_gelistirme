3a06540918e353eacf13eff2053637c99cdced8f
İbrahim Varola <varolaibrahim@gmail.com>
2020-04-25 16:01:39 +0300

    yeni dosya:   karakter_dizi_method2.py

---
+++ karakter_dizi_method2.py
@@ -0,0 +1,14 @@
+#upper() ve lower() metodları
+dizi1 = "IBRAHIM"
+dizi2 = "varola"
+
+print(dizi1.lower())
+print(dizi2.upper())
+
+#isupper() ve islower() metodları bize karakter dizisinin büyük veya küçük karakterlerden oluştuğunu belirtir.
+
+#endswith() metodu karakter dizisinin sonunu kontrol etmemizi sağlar. (syntax = endswith("son_kısım"))
+
+dizi3 = "nightwish - noise.flac"
+print(dizi3.endswith(".flac"))
+print(dizi3.endswith(".mp3"))
